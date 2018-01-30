73fe11d6f40bbb233b4b1d234c2dd2acac1921a3
btalar <bartek.talar@gmail.com>
2018-01-30 23:24:20 +0100

    rimart

--- index.php
+++ index.php
@@ -10,7 +10,22 @@
 <body>
 
     <!-- HOMEBANNER -->
-    <section class="home-banner" style="background: url(img/banner.png)">
+    <section class="banner" style="background: url(img/banner.png); background-size: cover; background-position: center top;">
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
     </section>
     <!-- END --------->
